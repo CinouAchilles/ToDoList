4c5a3fb1ba1131de183327d2a40743fdb4da7f8d
cinoudsk <yacinesa05@gmail.com>
2024-11-02 22:32:02 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import About from './componant/About';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/ToDoList">
       <div>
         <Nav />
         <Routes>
